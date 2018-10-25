0c9718df08dcf9338744ab00ede5048bebdc47f4
DC <dachang.h@gmail.com>
2018-10-25 10:26:35 +0800

    added products CRUD and controller specs

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -5,4 +5,15 @@ FactoryBot.define do
     price { Faker::Number.number(3) }
     published { false }
   end
+
+  factory :invalid_product, parent: :product do
+    name { nil }
+  end
+
+  factory :udpated_product, parent: :product do
+    name { Faker::Lorem.unique.word }
+    description { Faker::Lorem.unique.sentence(20) }
+    price { Faker::Number.unique.number(3) }
+    published { true }
+  end
 end
