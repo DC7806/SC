a3b0bbbedb24bdab50e68f55a58b8b73b2384fe8
DC <dachang.h@gmail.com>
2018-11-04 18:27:07 +0800

    Featuer/Add product to cart: models, views, routes

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,15 +4,17 @@ class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :products, through: :cart_items, dependent: :destroy
 
-  def add_product
-    item = cart_items.find_by(product_id: params[:id])
+  def add_product(product_id)
+    item = cart_items.find_by(product_id: product_id)
+    product_price = Product.find(product_id).price
 
     if item
       item.quantity += 1
       item.save
     else
       cart_items.create(cart_id: id,
-                        product_id: params[:id])
+                        product_id: product_id,
+                        unit_price: product_price)
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :products
+  resources :cart_items, only: [:create]
 end
